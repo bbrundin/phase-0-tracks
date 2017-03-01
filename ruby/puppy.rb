63e1697adc60a6af4dc4497914b27f1d0fd8720c
Brittany Brundin <bbrundin112@gmail.com>
2017-02-28 23:24:26 -0500

    created new puppy class for 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,29 @@
+
+# SPECIES
+# Extra Fuzzy Puppy (tm)
+
+# CHARACTERISTICS
+# Fur amount: Heavy
+# Friendly: yes
+# Color: varies
+# Legs: 4
+# Tail: yes
+
+
+# BEHAVIOR
+# Licks
+# Cuddles
+# Snores
+# Runs
+
+
+class Puppy
+end
+
+Puppy.methods
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+
+
