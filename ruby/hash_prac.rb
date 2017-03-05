8e57e772d084846a3c32149c01e8c90f6bf4afe9
Brittany Brundin <bbrundin112@gmail.com>
2017-03-05 16:16:01 -0500

    committing changes

--- ruby/hash_prac.rb
+++ ruby/hash_prac.rb
@@ -20,3 +20,23 @@ p application
 
 p application [:hire_status][:name] + hire_status[:address]
 
+
+# client = {
+#   name: "Miranda Priestly",
+#   client_age: "50",
+#   children_num: "2",
+#   decor_theme: "minimal glamour"
+#   nautical_pref: "no"
+# }
+
+# p client[:name]
+# client[:favshade] = "indigo"
+# p client
+# client[:wallpaper_pref] = "paisley"
+# client [:ombre_pref] = "fierce"
+# p client
+
+# p client[:hire_status] = "hired"
+# p client[:date_hired]= "6/02/16"
+# p client
+
