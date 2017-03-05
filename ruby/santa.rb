cb911ef46783bc499416c58f92d41e21663206ee
Brittany Brundin <bbrundin112@gmail.com>
2017-03-04 23:55:20 -0500

    finished release 4 created 100 santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,11 +1,18 @@
 class Santa
-    def initialize(gender, ethnicity, name)
-      @gender = gender
-      @ethnicity = ethnicity
-      @name = name
-      @age = 0
-      puts "Initializing Santa instance ..."
-    end
+
+  attr_reader :ethnicity
+  attr_accessor :age, :gender
+
+  def initialize(gender, ethnicity)
+    #passed in
+    @gender = gender
+    @ethnicity = ethnicity
+
+    #defaults
+    @age = 0
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    puts "Initializing Santa instance ..."
+  end
 
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
@@ -13,80 +20,26 @@ class Santa
 
   def eat_milk_and_cookies(treat)
     puts "that was a good #{treat}!"
-    treat
   end
 
   def celebrate_birthday
     @age += 1
   end
 
-  def getName
-    @name
+  def get_mad_at(reindeer)
+    @reindeer_ranking.delete(reindeer)
+    @reindeer_ranking.push(reindeer)
   end
 
-  def age
-    @age
-  end
-
-  def ethnicity
-    @ethnicity
-  end
-
-  def age=(new_age)
-    @age = new_age
-  end
-
-  def gender=(new_gender)
-    @gender = new_gender
-  end
-
-  def reindeer_ranking
-    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-  end
-
-  def get_mad_at(reindeer)  #take reindeer from reindeer ranking and make it index -1
-    # for reindeer in reindeer_ranking
-    index = [-1]
-    end
 end
 
 #Driver Code
+gender = ["Female", "Male", "Fluid", "Unicorn", "Agender"]
+ethnicity = ["Black", "White", "Mixed", "Hawaiian", "Latino", "Japanese", "Arcadian"]
 
-# santa.speak
-# santa.eat_milk_and_cookies("brownie")
-
-
-ethnicity = ["black", "white", "mixed", "hawaiian"]
-gender = ["female", "male", "fluid", "unicorns"]
-name = ["Nick", "Fred", "Luke", "Brad"]
-
-puts "Iterating through Santa ethnicities.."
-ethnicity.each do |ethnicity|
-  puts "Santas can be #{ethnicity}!"
+100.times do |i|
+  santa = Santa.new(gender.sample, ethnicity.sample)
+  santa.age = rand(140)
+  p "Santa's gender is #{santa.gender} and ethnicity is #{santa.ethnicity} and their age is #{santa.age}"
 end
-
-puts "Iterating through Santa genders.."
-gender.each do |gender|
-  puts "Santas can be #{gender}!"
-end
-
-puts "Iterating through Santas who have signed up for SantaCon.."
-name.each do |name|
-  puts "#{name} has signed up for SantaCon!"
-end
-
-santas = []
-
-ethnicity.length.times do |i|
-  santas << Santa.new(ethnicity[i], gender[i], name[i])
-end
-
-puts "Testing that each Santa instance in the array to make sure they can greet the kiddos.."
-santas.each do |santa|
-  p santa.getName
-  santa.speak
-end
-
-santas[0].celebrate_birthday
-
 
