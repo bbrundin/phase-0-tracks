039363a1949a9a3bb1cf6e7df8c66bf05d3b3994
Brittany Brundin <bbrundin112@gmail.com>
2017-03-12 04:18:16 -0400

    finished release 0 code and psuedocode

---
+++ js/algos.js
@@ -0,0 +1,34 @@
+// Release 0
+
+// psuedocode
+
+// we must create an array that accepts strings
+
+// we must call for a function that measures .length of every word in array
+
+// create a for loop that loops through each word in the array and measures length
+
+// returns the longest string in the array
+
+
+var words = ["hello", "hi", "howdy", "hola", "how you doin"];
+
+var abc = ["a", "b", "c", "abc"];
+
+function checkLongestWord(arr) {
+  var i;
+  var checkLength = 0;
+  var checkIndex = 0;
+  for (i = 0; i < arr.length; i++) {
+    var word = arr[i];
+    var length = word.length;
+    if (length > checkLength) {
+      checkLength = length;
+      checkIndex = i;
+    }
+  }
+  return arr[checkIndex];
+}
+
+console.log(checkLongestWord(words));
+console.log(checkLongestWord(abc));
