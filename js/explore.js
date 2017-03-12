0b559b6cae4bab5dd09c007ba1936cddb99d726e
Brittany Brundin <bbrundin112@gmail.com>
2017-03-12 03:11:43 -0400

    psuedocode for javascript reverse string program

---
+++ js/explore.js
@@ -0,0 +1,59 @@
+// Release 0
+// 1. null
+// 2. var someVariable - no spaces
+// 3. to print = console.log
+// 4. ending a line of code in javascript;
+// 5. // to comment
+
+// Release 1
+// 1. else if is two words
+// 2. brackets are around conditionals
+// 3. to print you use console.log
+
+// Release 2
+// 1. declare a function by writing function and adding brackets
+// 2. always have to write return if trying to get a value from a function- there is no implicit return
+// 3. When removing () the function returns error
+// 4. in Javascript a function call always needs parentheses
+
+// Release 3
+// 1. Loop counting to 10
+// var i = 10;
+
+// for (var i = 0; i < 10; i++) {
+//   console.log([i]);
+// }
+
+// 2. i+=2 runs an infinite Loop
+
+// Release 4
+
+// psuedocode
+
+// we must create a string
+
+// and then print it in reverse
+
+// create a for loop to loop through each word in our string
+
+// loop will continue count from top to bottom
+
+// use i-- on strings length to print in reverse
+
+
+// create function called reverseString
+
+// newString = "";
+
+// loop from end to beginning of string as letter
+
+// add letter to newString
+
+// return string
+
+
+
+
+
+
+
