5610b6abd44e4dfcb2e98b1a0af5d96d97458d54
Brittany Brundin <bbrundin112@gmail.com>
2017-03-13 10:36:57 -0400

    finished release 1

--- js/algos.js
+++ js/algos.js
@@ -35,6 +35,34 @@ console.log(checkLongestWord(abc));
 
 // Release 1
 
+// create a function that passes through two objects as parameters
+// each object has a key, value pair
+// we must compare keys in each object
+// if key1 is equal to key 2 and value 1 is equal to value 2
+//   return true if true
+// else return false
+
+// print new instance of findKeyValueMatch function
+// add Steven, age 54
+// add Tamir age 54
+
+// these share an age value so loop will catch value 1 == value 2 and return true
+
+// end
 
 
-// Release 2
+function findKeyValueMatch(obj1, obj2) {
+  for (var key1 in obj1) {
+    for (var key2 in obj2) {
+      var value1 = obj1[key1];
+      var value2 = obj2[key2];
+      if (key1 == key2 && value1 == value2) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(findKeyValueMatch({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+
