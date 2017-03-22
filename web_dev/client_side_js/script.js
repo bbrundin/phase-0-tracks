5db9c4315a843ffcb4ae39851d4ead9103452ed4
Brittany Brundin <bbrundin112@gmail.com>
2017-03-22 17:50:41 -0400

    added index for dog sheet and connected to javascript

---
+++ web_dev/client_side_js/script.js
@@ -0,0 +1,15 @@
+console.log("The script is running");
+
+
+function addPurpleBorder (event) {
+  console.log("click happened");
+  console.log(event);
+  event.target.style.border = "2px dashed purple";
+ }
+
+ // addPurpleBorder();
+
+var photo = document.getElementById("dog-wig-photo");
+photo.addEventListener("click", addPurpleBorder);
+
+
